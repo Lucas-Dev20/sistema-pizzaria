ba1f8222ed47493f46cb39fda1110f6ca61bbc4e
beatrizguimaraesdev <beatrizguimaraesdev@gmail.com>
2026-04-27 22:28:13 -0300

    feat: adiciona classe Pizza

---
+++ src/br/edu/ufersa/pizzaria/model/Pizza.java
@@ -0,0 +1,40 @@
+package br.edu.ufersa.pizzaria.model;
+
+public class Pizza
+{
+    private String tipo;
+    private double valor;
+
+
+    // Construtor
+    public Pizza (String tipo, double valor){
+        setTipo(tipo);
+        setValor(valor);
+    }
+
+
+    public double getValor() {
+        return valor;
+    }
+
+
+    public void setValor(double valor) {
+        if (valor > 0) {
+            this.valor = valor;
+        }
+    }
+
+
+    public String getTipo() {
+        return tipo;
+    }
+
+
+    public void setTipo(String tipo) {
+        if (tipo != null && !tipo.trim().isEmpty()) {
+            this.tipo = tipo;
+        }
+    }
+
+
+}
