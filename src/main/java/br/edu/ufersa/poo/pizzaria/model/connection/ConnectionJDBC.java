4e573450a2caa7a8ee7e0b0862f86b511bb377c7
Lucas Gabriel <lucasgabriel.sg323@gmail.com>
2026-05-31 17:35:25 -0300

    Update ConnectionJDBC.java
    vazou a senha

--- src/main/java/br/edu/ufersa/poo/pizzaria/model/connection/ConnectionJDBC.java
+++ src/main/java/br/edu/ufersa/poo/pizzaria/model/connection/ConnectionJDBC.java
@@ -7,8 +7,8 @@ public class ConnectionJDBC {
 
     private static final String URL =
             "jdbc:mysql://localhost:3306/pizzaria";
-    private static final String USER = "Lucas";
-    private static final String PASSWORD = "Lukah23@1";
+    private static final String USER = "user";
+    private static final String PASSWORD = "senha";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
