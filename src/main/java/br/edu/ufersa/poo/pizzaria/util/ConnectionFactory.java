d8815a4ce50c03eaa9c2eccd8edd90a55481c264
beatrizguimaraesdev <beatrizguimaraesdev@gmail.com>
2026-06-01 02:57:41 -0300

    adicionando ConectionFactory

---
+++ src/main/java/br/edu/ufersa/poo/pizzaria/util/ConnectionFactory.java
@@ -0,0 +1,27 @@
+package br.edu.ufersa.poo.pizzaria.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/pizzaria";
+
+    private static final String USER =
+            "root";
+
+    private static final String PASS =
+            "12345";
+
+    public static Connection getConnection()
+            throws SQLException {
+
+        return DriverManager.getConnection(
+                URL,
+                USER,
+                PASS
+        );
+    }
+}
